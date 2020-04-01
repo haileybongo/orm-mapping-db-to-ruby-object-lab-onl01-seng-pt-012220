6cf4def380fbb90db557857e0a1ae2b28a58bb66
Hailey Bongo <haileyebongo@gmail.com>
2020-04-01 19:45:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,16 +29,16 @@ class Student
   
   def self.new_from_db(row)
     new_student = self.new  
-    new_song.id = row[0]
-    new_song.name =  row[1]
-    new_song.length = row[2]
-    new_song  # return the newly created instance
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student  
   end
   
   def self.find_by_name(name)
       sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
       WHERE name = ?
       LIMIT 1
     SQL
